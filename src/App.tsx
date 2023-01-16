1154494248315f0d61cfaa81eac79e75c89f568d
Leandro Henrique <leandro2600@gmail.com>
2023-01-16 20:27:20 -0300

    Início da implementação do jogo da memória em React

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png';
+import { InfoItem } from './components/InfoItem';
+
+const App = () => {
+  return (
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} width="200" alt="" />
+        </C.LogoLink>
+        <C.InfoArea>
+          <InfoItem label='Tempo' value='00:00' />
+          <InfoItem label='Movimentos' value='0' />
+        </C.InfoArea>
+        <button>Reiniciar</button>
+      </C.Info>
+      <C.GridArea>
+        ...
+      </C.GridArea>
+    </C.Container>
+  );
+}
+
+export default App;
