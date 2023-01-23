885f40467fc2ef9868c8087926999db1c133a71d
Leandro Henrique <leandro2600@gmail.com>
2023-01-23 09:33:08 -0300

    Criação do componente GridItem e do helper para formatação do tempo

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { GridItem } from './components/GridItem';
 
 import { GridItemType } from './types/GridItemType';
 import { items } from './data/items';
+import { formatTimeElapsed } from './helpers/formatTimeElapsed';
 
 
 const App = () => {
@@ -21,6 +22,13 @@ const App = () => {
 
 
   useEffect(() => resetAndCreateGrid(), []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (playing) setTimeElapsed(timeElapsed + 1);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [playing, timeElapsed]);
 
   const resetAndCreateGrid = () => {
     // step 1 - resetar o jogo
@@ -52,7 +60,6 @@ const App = () => {
     // 2.3 - jogar no state
     setGridItems(tmpGrid);
 
-
     // passo 3 - começar o jogo
     setPlaying(true);
   
@@ -60,8 +67,7 @@ const App = () => {
 
   const handleItemClick = (index: number) => {
 
-  }
-  
+  }  
 
   return (
     <C.Container>
@@ -70,7 +76,7 @@ const App = () => {
           <img src={logoImage} width="200" alt="" />
         </C.LogoLink>
         <C.InfoArea>
-          <InfoItem label='Tempo' value='00:00' />
+          <InfoItem label='Tempo' value={formatTimeElapsed(timeElapsed)} />
           <InfoItem label='Movimentos' value='0' />
         </C.InfoArea>
 
