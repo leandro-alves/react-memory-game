20ce8160da2dfcf6dd327fc8a1d38e9cbcd38ec0
Leandro Henrique <leandro2600@gmail.com>
2023-01-25 16:04:17 -0300

    Finalizando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,46 @@ const App = () => {
     return () => clearInterval(timer);
   }, [playing, timeElapsed]);
 
+  // Verify if opened are equal
+  useEffect(() => {
+    if(shownCount === 2) {
+      let opened = gridItems.filter(item => item.shown === true);
+      if(opened.length === 2) {
+        if(opened[0].item === opened[1].item) {
+          // v1 - if both are equal, make every "shown" permanent
+          let tmpGrid = [...gridItems];
+          for(let i in tmpGrid) {
+            if(tmpGrid[i].shown) {
+              tmpGrid[i].permanentShown = true;
+              tmpGrid[i].shown = false;
+            }
+          }
+          setGridItems(tmpGrid);
+          setShownCount(0);
+        } else {
+          // v2 - if they are NOT equal, close all "shown"
+          setTimeout(() => {
+            let tmpGrid = [...gridItems];
+            for(let i in tmpGrid) {
+              tmpGrid[i].shown = false;
+            }
+            setGridItems(tmpGrid);
+            setShownCount(0);
+          }, 1000);
+        }
+
+        setMoveCount(moveCount => moveCount + 1);
+      }
+    }
+
+  }, [shownCount, gridItems]);
+
+  useEffect(() => {
+    if(moveCount > 0 && gridItems.every(item => item.permanentShown === true)) {
+      setPlaying(false);
+    }
+  }, [moveCount, gridItems]);
+
   const resetAndCreateGrid = () => {
     // step 1 - resetar o jogo
     setTimeElapsed(0);
@@ -66,7 +106,14 @@ const App = () => {
   }
 
   const handleItemClick = (index: number) => {
-
+    if(playing && index !== null && shownCount < 2) {
+      let tmpGrid = [...gridItems];
+      if(tmpGrid[index].permanentShown === false && tmpGrid[index].shown === false) {
+        tmpGrid[index].shown = true;
+        setShownCount(shownCount + 1);
+      }
+      setGridItems(tmpGrid);
+    }
   }  
 
   return (
@@ -77,7 +124,7 @@ const App = () => {
         </C.LogoLink>
         <C.InfoArea>
           <InfoItem label='Tempo' value={formatTimeElapsed(timeElapsed)} />
-          <InfoItem label='Movimentos' value='0' />
+          <InfoItem label='Movimentos' value={moveCount.toString()} />
         </C.InfoArea>
 
         <Button label='Reiniciar' icon={RestartIcon} onClick={resetAndCreateGrid} />
